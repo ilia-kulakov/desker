dc56e60332c20a45af6cd0713356f270c8bc2a6e
Ilia Kulakov <Ilia_Kulakov@epam.com>
2023-11-14 13:57:22 +0100

    Initialization

---
+++ src/main/java/jax/spring/desker/DeskerApplication.java
@@ -0,0 +1,13 @@
+package jax.spring.desker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jax/spring/desker/DeskerApplicationTests.java
@@ -0,0 +1,13 @@
+package jax.spring.desker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
